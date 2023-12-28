6d44f4e0326f2a2301adfdff5d141e4045aea4d9
Nikhil-Dsouza-15 <nikhil.kr.dsouza9090@gmail.com>
2023-12-28 17:58:53 +0530

    ok

--- index.js
+++ index.js
@@ -7,15 +7,48 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
+  // Sending HTML content dynamically
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ritesh's Dog Adoption Page</title>
+    </head>
+    <body>
+    
+    <h1>Ritesh's Dog Adoption Page</h1>
+    
+    <ul>
+        <!-- Golden Retriever -->
+        <li>
+            <a href="https://en.wikipedia.org/wiki/Golden_Retriever" target="_blank">
+                <img src="img link for Golden Retriever" alt="Golden Retriever Image">
+            </a>
+        </li>
+    
+        <!-- Poodle -->
+        <li>
+            <a href="https://en.wikipedia.org/wiki/Poodle" target="_blank">
+                <img src="img link for Poodle" alt="Poodle Image">
+            </a>
+        </li>
+    </ul>
+    
+    </body>
+    </html>
+  `;
+
+  res.send(htmlContent);
 });
-//your code here
+
 app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
+  const { a, b } = req.body;
+  res.status(200).send(`${a + b}`);
 });
+
 module.exports = app;
